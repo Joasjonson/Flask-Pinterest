2a47aed07b2fc5f99fe4ee22db68db6a7fd830e1
Joás Jonson <joasjonson@gmail.com>
2024-03-13 20:30:07 +0100

    add psycopg2

--- mypinterest/__init__.py
+++ mypinterest/__init__.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("db_url")
 app.config["SQLALCHEMY_DATABASE_URI"] = "mssql+pyodbc://@DESKTOP-DU0URSN/TESTE?driver=ODBC+Driver+17+for+SQL+Server"
 app.config["SECRET_KEY"] = "43a20b740bed232291d874980fe79f"
 app.config["UPLOAD_FOLDER"] = "static/picture_posts"
